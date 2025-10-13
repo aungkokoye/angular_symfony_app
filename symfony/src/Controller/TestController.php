559019669a79b8ccb0d60dced37716373f484d79
Aung Ko Ko Ye <aye@high5hire.com>
2025-10-13 19:58:26 +0100

    added test symfony mailer controller

--- symfony/src/Controller/TestController.php
+++ symfony/src/Controller/TestController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class TestController extends AbstractController
@@ -17,6 +19,24 @@ final class TestController extends AbstractController
     {
         return $this->json([
             'message' => 'Test endpoint is working!',
+            'status' => 'success',
+        ]);
+    }
+
+    #[Route('/notification', name: 'app_notification')]
+    public function notification(MailerInterface $mailer): Response
+    {
+        $email = (new Email())
+            ->from('hello@example.com')
+            ->to('sender@example.com')
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $mailer->send($email);
+
+        return $this->json([
+            'message' => 'Test Symfony mailer sending email endpoint is working!',
             'status' => 'success',
         ]);
     }
